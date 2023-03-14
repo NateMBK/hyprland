97f91f400d49fd96ae01313f4d16df170bd848f7
Nate <gtanatembk@hotmail.com>
2023-03-13 17:47:44 -0700

    Create hyprland.sh

---
+++ hyprland.sh
@@ -0,0 +1,6 @@
+yes | sudo pacman -S git
+git clone https://aur.archlinux.org/yay-bin
+cd yay-bin
+makepkg -si
+
+yay -S hyprland-bin polkit-gnome ffmpeg neovim viewnior dunst rofi pavucontrol thunar starship wl-clipboard wf-recorder swaybg grimblast-git ffmpegthumbnailer tumbler playerctl noise-suppression-for-voice thunar-archive-plugin kitty waybar-hyprland wlogout swaylock-effects sddm-git nwg-look-bin nordic-theme papirus-icon-theme pamixer

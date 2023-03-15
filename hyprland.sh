7213bd13c918bf0b9338500e2d270ec0f37b46ac
Nate <gtanatembk@hotmail.com>
2023-03-14 17:24:53 -0700

    updated (applies hyprland config and reboots)

--- hyprland.sh
+++ hyprland.sh
@@ -1,3 +1,3 @@
 yay -S hyprland-bin polkit-gnome ffmpeg neovim viewnior rofi pavucontrol thunar starship wl-clipboard wf-recorder swaybg grimblast-git ffmpegthumbnailer tumbler playerctl noise-suppression-for-voice thunar-archive-plugin kitty waybar-hyprland wlogout swaylock-effects sddm-git pamixer nwg-look-bin nordic-theme papirus-icon-theme dunst ttf-nerd-fonts-symbols-common otf-firamono-nerd
-cp -r hyprland/hyprland/dotconfig/* /home/nate/.config/
-sudo systemctl enable sddm && sudo systemctl start sddm
+cp -r hyprland/dotconfig/* /home/nate/.config/
+sudo systemctl enable sddm && sudo reboot
